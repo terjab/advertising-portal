b928cb92530e83cbb56e6500e2e793133e241a1b
Tereza Jabornická <t.jabornicka@gmail.com>
2021-07-26 23:09:49 +0200

    Changes

---
+++ src/api/ads/create-ad.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { URL } from '../../../'
+
+export const CreateAd = async (ad) => {
+  const response = await fetch(`${URL}/ad`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(ad),
+  })
+
+  console.log(response)
+}
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const URL = "localhost:38910/api"
